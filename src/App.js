925a573e04c78401d3873e528ef9f1aea0281a6f
JinYong <1520650026@qq.com>
2019-10-21 11:22:22 +0800

    tj

---
+++ src/App.js
@@ -0,0 +1,22 @@
+/* 
+应用根组件
+*/
+import React, {Component} from 'react'
+import {HashRouter,Route, Switch} from 'react-router-dom'
+
+import Login from "./pages/login/Login"
+import Admin from "./pages/admin/Admin"
+
+export default class App extends Component {
+
+  render () {
+    return (
+      <HashRouter>
+        <Switch> {/* /login/xxx   默认使用不完全匹配 | 使用第一个匹配的路由 */}
+          <Route path="/login" component={Login}/>
+          <Route path="/" component={Admin}/>
+        </Switch>
+      </HashRouter>
+    )
+  }
+}
