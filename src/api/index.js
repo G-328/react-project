a148441116b061b1a8416842ae10c612b3a65d52
JinYong <1520650026@qq.com>
2019-10-27 14:56:28 +0800

    l

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,8 @@
 //包含n个接口函数的模块
 import ajax from './ajax'
+import jsonp from 'jsonp'
+// import { resolve } from 'url'
+import { message } from 'antd'
 //登录的
 export const reqLogin = ({username,password}) => ajax({
     url: '/login',
@@ -12,3 +15,23 @@ export const reqUsers =() => ajax({
   url:'/manage/user/list',
   method: 'GET'
 })
+
+//封装获取指定城市的天气信息
+export const reqWeather = (city) => {
+  return new Promise((resolve,reject) => {
+    const url=`http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+    jsonp(url,{},(err,data) => {
+      console.log(err,data)
+      if(!err && data.status==='success'){
+      const {dayPictureUrl,weather} = data.results[0].weather_data[0]
+      resolve({dayPictureUrl,weather})
+    }else {
+      // reject(new Error('获取天气异常!'))
+      message.error('获取天气失败!')
+      return new Promise(()=>{})
+    }
+  })
+  })
+}
+
+reqWeather('北京')
--- src/containers/admin/header/index.jsx
+++ src/containers/admin/header/index.jsx
@@ -2,20 +2,56 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import dayjs from 'dayjs'
+import {Modal,Button,Icon} from 'antd'
+import screenfull from 'screenfull'
 import LinkButton from '../../../components/link-button'
+import {removeUserToken} from '../../../redux/action-creators/user'
+import {reqWeather} from '../../../api'
+
 
 import './index.less'
+import { isFulfilled } from 'q'
 
-@connect(state => ({ username: state.user.user.username }))
+@connect(
+  state => ({ username: state.user.user.username }),
+  {removeUserToken}
+)
 @withRouter //作用：包装非路由组件使其拥有路由组件的功能，但返回的组件不是路由组件。向组件内部传入三个属性:history、location、match 
 class Header extends Component {
-
   state = {
-    currentTime: dayjs().format('YYYY-MM-DD HH:mm:ss')
-
+    currentTime: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+    dayPictureUrl:'',
+    weather:'',
+    isFullScreen:false,//当前是否全屏显示
   }
+
   logout = () => {
-    alert(1)
+  const that = this
+    Modal.confirm({
+      title:'确认退出吗',
+      onOk(){
+        that.props.removeUserToken()
+      },
+      onCancel(){
+        console.log('cancel')        
+      }
+    })
+  }
+
+  handleFullScreen = () =>{
+    if(screenfull.isEnabled){
+      screenfull.toggle();
+      this.setState
+    }
+  }
+
+  showWeather = async () => {
+    //请求数据
+    const {dayPictureUrl,weather}= await reqWeather('北京')
+    this.setState({
+      dayPictureUrl,
+      weather
+    })
   }
   componentDidMount() {
     //启动循环定时器,每隔一秒，更新时间
@@ -24,6 +60,8 @@ class Header extends Component {
         currentTime: dayjs().format('YYYY-MM-DD HH:mm:ss')
       })
     }, 1000);
+    //请求获取天气信息
+    this.showWeather()
   }
 
   componentWillUnmount() {
@@ -33,10 +71,13 @@ class Header extends Component {
   render() {
     //得到当前请求的路由路径
     const path = this.props.location.pathname
-    const { currentTime } = this.state
+    const { currentTime,dayPictureUrl,weather } = this.state
     return (
       <div className="header">
         <div className="header-top">
+          <Button size="small" onClick={this.handleFullScreen}>
+          <Icon type={isFullScreen ? 'fullscreen-exit' : 'fullscreen'} />
+          </Button> &nbsp;
           <span>欢迎，{this.props.username}</span>
           <LinkButton onClick={this.logout}>退出</LinkButton>
         </div>
@@ -44,8 +85,8 @@ class Header extends Component {
           <div className="header-bottom-left">{path}</div>
           <div className="header-bottom-right">
             <span>{currentTime}</span>
-            <img src="http://api.map.baidu.com/images/weather/day/xiaoyu.png" alt="weather" />
-            <span>小雨转多云</span>
+            <img src={dayPictureUrl} alt="weather" />
+            <span>{weather}</span>
           </div>
         </div>
       </div>
