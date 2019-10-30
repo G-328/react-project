3c0e50e5af47b6fb76c239066585b478ba9fa38a
JinYong <1520650026@qq.com>
2019-10-30 18:54:32 +0800

    l

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 应用根组件
 */
 import React, {Component} from 'react'
-import {Router,Route, Switch} from 'react-router-dom'
+import {Router,Route, Switch, Redirect} from 'react-router-dom'
 
 import Login from "./containers/login"
 import Admin from "./containers/admin"
 import history from './history'
+import routes from './config/routes'
 
 export default class App extends Component {
 
@@ -15,7 +16,16 @@ export default class App extends Component {
       <Router history={history}>
         <Switch> {/* /login/xxx   默认使用不完全匹配 | 使用第一个匹配的路由 */}
           <Route path="/login"  component={Login} exact />
-          <Route path="/" component={Admin}/>
+          <Admin>
+            <Switch>
+              {
+                routes.map(item => (
+                  <Route {...item} key={item.path}/>
+                ))
+              }
+              <Redirect to="/home"/>
+            </Switch>
+          </Admin>
         </Switch>
       </Router>
     )
--- src/containers/admin/index.jsx
+++ src/containers/admin/index.jsx
@@ -4,21 +4,11 @@
 
 import React, { Component } from 'react'
 import { Layout } from 'antd'
-import { Route, Switch, Redirect } from 'react-router-dom'
 
 import LeftNav from './left-nav'
 import AdminHeader from './header'
 import WithChenkLogin from '../with-check-login'
-import Home from '../../components/home'
-import Bar from '../../components/charts/bar'
-import Line from '../../components/charts/line'
-import Pie from '../../components/charts/pie'
-import User from '../user'
-import Role from '../role'
-import ProductList from '../product'
-import ProductDetail from '../product/detail'
-import ProductAddUpdate from '../product/add-update'
-import Category from '../category'
+
 
 
 const { Footer, Sider, Content } = Layout
@@ -35,19 +25,9 @@ class Admin extends Component {
         <Layout>
           <AdminHeader />
           <Content style={{ backgroundColor: 'white', margin: '30px 15px 0' }}>
-            <Switch>
-              <Route path="/home" component={Home} exact />
-              <Route path="/category" component={Category} exact />
-              <Route path="/product/" component={ProductList} exact />
-              <Route path="/product/detail/:id" component={ProductDetail} />
-              <Route path="/product/addupdate" component={ProductAddUpdate} />
-              <Route path="/role" component={Role} />
-              <Route path="/user" component={User} />
-              <Route path="/charts/line" component={Line} />
-              <Route path="/charts/bar" component={Bar} />
-              <Route path="/charts/pie" component={Pie} />
-              <Redirect to="/home" />
-            </Switch>
+            {
+              this.props.children
+            }
           </Content>
           <Footer style={{ textAlign: 'center' }}>推荐使用谷歌浏览器，可以获得更佳页面操作体验</Footer>
         </Layout>
